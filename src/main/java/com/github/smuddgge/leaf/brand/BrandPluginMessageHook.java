1027415bfa7dda7ba73bd3c01ac6be878bf8a140
R00tB33rMan <36140389+R00tB33rMan@users.noreply.github.com>
2024-02-07 15:51:53 +0000

    Is Retention More Favorable?

--- src/main/java/com/github/smuddgge/leaf/brand/BrandPluginMessageHook.java
+++ src/main/java/com/github/smuddgge/leaf/brand/BrandPluginMessageHook.java
@@ -40,7 +40,7 @@ class BrandPluginMessageHook extends PluginMessagePacket {
 
         // Check if the handler is the correct handler.
         // Check if the brand feature is enabled.
-        if (!(this.enabled && PluginMessageUtil.isMcBrand(this))
+        if (!(handler instanceof BackendPlaySessionHandler)
                 || !ConfigMain.get().getBoolean("brand.in_game.enabled", false)
                 || !PluginMessageUtil.isMcBrand(this)) {
 
